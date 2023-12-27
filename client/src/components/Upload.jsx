7e5a03e69626958591abd9efb3a14cc0da3873ca
thanhgit123 <hai880345@gmail.com>
2023-12-27 20:01:31 +0700

    add

--- client/src/components/Upload.jsx
+++ client/src/components/Upload.jsx
@@ -69,55 +69,99 @@ export default function Upload() {
       alert(error.response.data.message);
     }
   };
+  const handleSave = async () => {
+    try {
+      const response = await publicAxios.put(
+        `/api/v1/products/${newProduct.product_id}`,
+        newProduct
+      );
+
+      setProducts(response.data.products);
+    } catch (error) {}
+  };
+  const [choose, setChoose] = useState("category");
+  const handleEdit = async (item) => {
+    const response = await publicAxios.get(
+      `/api/v1/editProduct/${item.product_id}`
+    );
+    setChoose(response.data.data.name_category);
+    setNewProduct(response.data.data);
+  };
+
   return (
     <>
       <h1>Upload</h1>
-      <div>
-        <input type="file" onChange={handleAddMedia} />
-        <img src={preview} alt="" width={100} />
-        <select name="category_id" onChange={handleGetValue}>
-          <option value="">Chọn thể loại</option>
-          {categories.map((category) => (
-            <option value={category.category_id}>
-              {category.name_category}
-            </option>
-          ))}
-        </select>
-        <br />
-        <label htmlFor="">Name:</label>
-        <input
-          type="text"
-          className="border border-sky-500"
-          name="name_product"
-          onChange={handleGetValue}
-        />
-        <br />
-        <label htmlFor="">Price:</label>
-        <input
-          type="text"
-          className="border border-sky-500"
-          name="price"
-          onChange={handleGetValue}
-        />
-        <br />
-        <label htmlFor="">Description:</label>
-        <input
-          type="text"
-          className="border border-sky-500"
-          name="description"
-          onChange={handleGetValue}
-        />
-        <br />
-        <label htmlFor="">Stock:</label>
-        <input
-          type="text"
-          className="border border-sky-500"
-          name="stock"
-          onChange={handleGetValue}
-        />
-        <br />
+      <div className="w-full flex">
+        <div className="w-1/3">
+          <input type="file" onChange={handleAddMedia} />
+          <img src={preview} alt="" width={100} />
+          <select name="category_id" onChange={handleGetValue}>
+            <option value="">{choose}</option>
+            {categories.map((category) => (
+              <option value={category.category_id}>
+                {category.name_category}
+              </option>
+            ))}
+          </select>
+          <br />
+          <label htmlFor="">Name:</label>
+          <input
+            type="text"
+            className="border border-sky-500"
+            name="name_product"
+            onChange={handleGetValue}
+            value={newProduct.name_product}
+          />
+          <br />
+          <label htmlFor="">Price:</label>
+          <input
+            type="text"
+            className="border border-sky-500"
+            name="price"
+            onChange={handleGetValue}
+            value={newProduct.price}
+          />
+          <br />
+          <label htmlFor="">Description:</label>
+          <input
+            type="text"
+            className="border border-sky-500"
+            name="description"
+            onChange={handleGetValue}
+            value={newProduct.description}
+          />
+          <br />
+          <label htmlFor="">Stock:</label>
+          <input
+            type="text"
+            className="border border-sky-500"
+            name="stock"
+            onChange={handleGetValue}
+            value={newProduct.stock}
+          />
+          <br />
 
-        <button onClick={handleAdd}>Add</button>
+          <button onClick={newProduct.product_id ? handleSave : handleAdd}>
+            Add
+          </button>
+        </div>
+        <div className="w-2/3">
+          <table></table>
+          {products.map((item, index) => (
+            <div key={index} className="flex">
+              <img src={item.image} alt="" width={100} />
+              <p>{item.name_product}</p>
+              <p>{item.price}</p>
+              <p>{item.description}</p>
+              <p>{item.category_id}</p>
+              <p>{item.stock}</p>
+              <button className="px-[20px]" onClick={() => handleEdit(item)}>
+                sua
+              </button>
+              <button>xoa</button>
+            </div>
+          ))}
+        </div>
       </div>
     </>
   );
